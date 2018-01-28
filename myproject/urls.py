c9c427c392502012dea5bbf2f7548b5cc88bb7d4
Theresia Kosche <th.kosche@gmail.com>
2018-01-28 16:04:36 +0000

    implemented logout and login and cleaned up board tests

--- myproject/urls.py
+++ myproject/urls.py
@@ -15,6 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.contrib.auth import views as auth_views
 
 from boards import views
 from accounts import views as accounts_views
@@ -23,6 +24,8 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name='home'),
     path('signup/', accounts_views.signup, name='signup'),
+    path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('boards/<int:pk>/', views.board_topics, name='board_topics'),
     path('boards/<int:pk>/new/', views.new_topic, name='new_topic'),
 ]
